dfe0b030d15d2b11eded2bbd6e0784731ffc13a3
bellex0 <bellepx3@gmail.com>
2019-10-08 17:43:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", handleForm)
+
+ function handleForm(evt){ 
+   evt.preventDefault()
+   let form = evt.target
+   let newTaskInput = form.newtaskdescription
+   let newTask = newTaskInput.value
+
+   let newTaskLi = document.createElement("li")
+
+   let buttonX = document.createElement("button")
+   buttonX.innerText = "X"
+
+   buttonX.addEventListener("click", function(e) {
+     e.target.parentNode.remove()
+   })
+
+   newTaskLi.innerText = newTask
+   newTaskLi.append(buttonX)
+
+let taskColorInput = form.priority
+let taskColor = taskColorInput.value
+
+
+  
+   newTaskLi.style.color = taskColor
+   document.querySelector("#tasks").append(newTaskLi)
+ }
+
 });
+
+
+
